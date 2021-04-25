a88eac076b00c7ac8364b4d1eb6c9e93c2f4ce59
toki <shoya.0120.t@gmail.com>
2021-04-25 11:28:47 +0000

    Heroku

--- php_practice/practice.php
+++ php_practice/practice.php
@@ -1,39 +1,35 @@
 <?php
 //課題１
-$a = 3;
-$b = 7;
-echo $a+$b;
+$name = "tokita";
+if($name == "tokita"){
+  echo "私はtokitaです。";
+} else {
+  echo "私はtokitaではありません。";
+}
 echo "\n";
 //課題２
-$array_month = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"];
-echo $array_month [7];
+$total =0;
+for($i =1;$i <=10000;$i++){
+  $total += $i;
+}
+echo $total;
 echo "\n";
-//課題３
-$hello = "hello, ";
-$name = "tokita";
-$world = "'s world!";
-echo $hello . $name . $world;
-echo "\n";
-//課題４
-$tech_boost = "tech";
-$tech_boost .= " boost";
-echo $tech_boost;
-echo "\n";
-//課題5
-$calendar_2018 = [
-  "January" => "1月",
-  "February" => "2月",
-  "March" => "3月",
-  "April" => "4月",
-  "May" => "5月",
-  "June" => "6月",
-  "July" => "7月",
-  "August" => "8月",
-  "September" => "9月",
-  "October" => "10月",
-  "November" => "11月",
-  "December" => "12月"
-];
+//課題3
+$fruits = array("apple","banana","grape","peach","lemon");
+foreach($fruits as $fruits){
+  echo "要素は" . $fruits;
+  echo "\n";
+}
+"\n";
+//課題4
+/* for文の始めの値を定義する */
+$start = 1;
+/* for文の終わりの値を定義する */
+$end = 100;
 
-// 12月を表示する
-echo $calendar_2018["December"];
+for($i = $start; $i <= $end; $i++){
+
+  // 5で割り切れたら{}内を実行する
+  if($i % 5 == 0)
+   echo "$i"."\n";
+  }
